56048e6dd9b87114312d5d709bd9fd0cdfd8d951
Juan <juanberesiarte@gmail.com>
2023-10-05 21:23:13 -0300

    Creando una clase e interfaz para los productos
    Creo una clase para manejar los productos en el programa y creo su respectiva interfaz

---
+++ src/main/java/com/codipro/holamundoanimal/producto/IProducto.java
@@ -0,0 +1,11 @@
+package com.codipro.holamundoanimal.producto;
+
+public interface IProducto {
+    
+    String obtenerNombre();
+
+    String obtenerDetalle();
+
+    int obtenerPrecio();
+    
+}
---
+++ src/main/java/com/codipro/holamundoanimal/producto/Producto.java
@@ -0,0 +1,43 @@
+package com.codipro.holamundoanimal.producto;
+
+/**
+ *
+ * @author juanb
+ */
+public class Producto implements IProducto {
+    
+    private String nombre;
+    private String detalle;
+    private int precio;
+
+    public Producto definirNombre(String nombre) {
+        this.nombre = nombre;
+        return this;
+    }
+
+    public Producto definirDetalle(String detalle) {
+        this.detalle = detalle;
+        return this;
+    }
+
+    public Producto definirPrecio(int precio) {
+        this.precio = precio;
+        return this;
+    }
+
+    @Override
+    public String obtenerNombre() {
+        return this.nombre;
+    }
+
+    @Override
+    public String obtenerDetalle() {
+        return this.detalle;
+    }
+
+    @Override
+    public int obtenerPrecio() {
+        return this.precio;
+    }
+    
+}
