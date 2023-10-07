76ece222d6d3f3d4740086b538c6c3a5f9b01a5a
Juan <juanberesiarte@gmail.com>
2023-10-06 23:04:41 -0300

    Cambie el enum de Localidades por clases
    Definí una clase Localidad y una clase Localidades que registra todas las Localidades usadas en el programa

--- src/main/java/com/codipro/holamundoanimal/tienda/Localidades.java
+++ src/main/java/com/codipro/holamundoanimal/tienda/Localidades.java
@@ -1,25 +1,23 @@
 package com.codipro.holamundoanimal.tienda;
 
-public enum Localidades {
-    
-    MAIPU("Maipu", 5515, 1000),
-    GUAYMALLEN("Guaymallen", 5521, 1200),
-    GODOY_CRUZ("Godoy Cruz", 5501, 1300),
-    MENDOZA("Mendoza", 5500, 1400),
-    LAS_HERAS("Las Heras", 5539, 1550),
-    LUJAN_DE_CUYO("Lujan de cuyo", 5507, 1700),
-    SAN_RAFAEL("San Rafael", 5600, 0),
-    MALARGUE("Malargue", 5613, 0),
-    GENERAL_ALVEAR("General Alvear", 5620, 0);
+public class Localidades {
 
-    String nombre;
-    int codigo_postal;
-    int coste_de_envio;
-
-    Localidades(String nombre, int codigo_postal, int coste_de_envio) {
-        this.nombre = nombre;
-        this.codigo_postal = codigo_postal;
-        this.coste_de_envio = coste_de_envio;
+    public static Localidad[] get(){
+        
+        Localidad[] localidades = new Localidad[]{
+            new Localidad("Maipu", 5515, 1000),
+            new Localidad("Guaymallen", 5521, 1200),
+            new Localidad("Godoy Cruz", 5501, 1300),
+            new Localidad("Mendoza", 5500, 1400),
+            new Localidad("Las Heras", 5539, 1550),
+            new Localidad("Lujan de cuyo", 5507, 1700),
+            new Localidad("San Rafael", 5600, 0),
+            new Localidad("Malargue", 5613, 0),
+            new Localidad("General Alvear", 5620, 0)
+        };
+        
+        return localidades;
+        
     }
     
 }
